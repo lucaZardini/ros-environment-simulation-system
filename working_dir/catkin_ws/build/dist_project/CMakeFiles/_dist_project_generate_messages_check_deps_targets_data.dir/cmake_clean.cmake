59a0efc228b7e4a8a9c1d2abbc0445a2c45db1e9
lucaZardini <luca.zardini@studenti.unitn.it>
2025-01-03 16:05:45 +0100

    Added built files

---
+++ working_dir/catkin_ws/build/dist_project/CMakeFiles/_dist_project_generate_messages_check_deps_targets_data.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_dist_project_generate_messages_check_deps_targets_data"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/_dist_project_generate_messages_check_deps_targets_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ working_dir/catkin_ws/devel/share/common-lisp/ros/dist_project/msg/_package_targets_data.lisp
@@ -0,0 +1,6 @@
+(cl:in-package dist_project-msg)
+(cl:export '(ROBOT_ID-VAL
+          ROBOT_ID
+          POINTS-VAL
+          POINTS
+))
